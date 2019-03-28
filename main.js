0fa8ee4c69a0baebf6dc3af76c957b01ab168df1
gpw <gpw#9393>
2019-03-28 16:27:40 +0000

    Core finished

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ const Discord = require('discord.js');
 const RCon = require('./rcon');
 const RConConf = require('./config/rcon');
 
+const DiscordMessage = require('./class/bridge/discord_message.js');
+const DiscordUpdate = require('./class/bridge/discord_update.js');
+const RConMessage = require('./class/bridge/rcon_message.js');
+const RConResponse = require('./class/bridge/rcon_response.js');
+
 var running = false;
 function log(msg) { console.log('[' + (new Date()).toJSON().slice(0, 19).replace(/[-T]/g, ':') + '] ', msg); }
 
 const rcon = new RCon(RConConf.host, RConConf.port, RConConf.pass);
 const discord = new Discord.Client();
 
+// If RCon crash, better just exit and make use of cron reboot since it wont reconnect on its own
 discord.on('error', function(e) { log('DISCORD error'); log(e); });
 rcon.on('error',    function(e) { log('RCON    error'); log(e); process.exit(); });
 
@@ -28,4 +34,9 @@ discord.on('ready', function() {
 
 rcon.connect();
 log('RCON    init');
+
+rcon.on('message', (m) => RConMessage(m));
+rcon.on('response', (m) => RConResponse(m));
+discord.on('message', (m) => DiscordMessag(m));
+discord.on('update', (m) => DiscordUpdate(m));
 
