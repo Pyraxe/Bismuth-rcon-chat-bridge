b47f99e15602a81e6c9475d98dc1c67eda36acc7
gpw <gpw#9393>
2019-03-28 17:49:11 +0000

    Discord > rcon should be 100% done

--- class/bridge/discord_to_rcon.js
+++ class/bridge/discord_to_rcon.js
@@ -1,3 +1,55 @@
 module.exports = function(discord, rcon, m, old = null) {
+  var tellraw = [
+    '<', { text:'@', color:'gray' }, (m.member ? m.member.displayName : m.author.username) + '> ',
+    format(discord, m) ];
+  if (old) {
+    tellraw.push({ text:' (old: ', color: 'gray' });
+    tellraw.push(format(discord, old, 'gray'));
+    tellraw.push({ text:')', color: 'gray' });
+  }
+
+  rcon_send('/tellraw @a [' + JSON.stringify(tellraw) + ']');
   return;
+};
+
+function format(discord, m, color = 'white') {
+  if (!m || !m.content)
+    return "";
+  var r = [], tmp = m.content.split(/(<[^<> ]*[0-9]+>)/);
+
+  // Let's try to find any mentions in order to color them
+  for (var t of tmp) {
+    if  (t[0] != '<' || t[t.length - 1] != '>' || t.length < 15)
+      r.push({ text:t, color: color });
+    else  {
+      var snowflake = t.match(/([0-9]+)>$/);
+      switch (t[1]) {
+        case '#':
+          var c = discord.channels.get(snowflake[1]);
+          r.push(c && c.name ? { text:'#' + c.name, color:'aqua' } : { text:t, color: color });
+          break;
+        case'@':
+          if (t[2] == '&') {
+            var role = m.channel.guild.roles.get(snowflake[1]);
+            r.push(role ? { text:'@' + role.name, color:'aqua' } : { text:t, color: color });
+            break;
+          }
+          var member = m.channel.guild.members.get(snowflake[1]);
+          if (member) {
+            r.push({ text:'@' + member.displayName, color:'aqua' });
+            break;
+          }
+          member = discord.users.get(snowflake[1]).username;
+          r.push(member ? { text:'@' + member.username, color:'aqua' } : { text:t, color: color });
+          break;
+        case ':':
+          r.push(t.substring(1,t.length - snowflake[1].length - 1));
+          break;
+        default:
+          r.push({ text:t, color: color });
+      }
+    }
+  }
+  return r;
 }
+
--- class/bridge/discord_update.js
+++ class/bridge/discord_update.js
@@ -2,7 +2,7 @@
  * Message coming from DISCORD and we want to analyze and maybe send it to RCON
  */
 module.exports = function(discord, rcon, o, m) {
-  if (!m.guild || m.author.bot)
+  if (!m.guild || m.author.bot || o.content == m.content)
     return;
   //Chat bridge only  happens in one channel
   if (m.channel.id == ConfDiscord.chat_bridge_channel)
