2395c9a18b51d591a80663d811fc4b0281bac97e
gpw <gpw#9393>
2019-03-29 02:38:40 +0000

    scoreboard display improvement

--- cmd/scoreboard.js
+++ cmd/scoreboard.js
@@ -22,28 +22,34 @@ module.exports = class Scoreboard extends Cmd {
       this.send_channel(m.channel, "`No objective was found by the name '" + obj + "'`");
       return true;
     }
-    var r = [''], total = 0, padLen = 3, s;
+    var r = [''], total = 0, padLen = 3, s, totLen = 0;
     if (table.length > 0) {
       s = table[0].score.toLocaleString();
       padLen = s.length + 2;
       for (var entry of table) {
-        r.push(entry.score.toLocaleString().padStart(padLen) + ' ' + entry.name);
+        totLen += r[r.push(entry.score.toLocaleString().padStart(padLen) + ' | ' + entry.name) - 1].length;
         total += entry.score;
       }
     }
-    r[0]= total.toLocaleString().padStart(padLen) + ' TOTAL';
+    r[0]= total.toLocaleString().padStart(padLen) + ' | TOTAL';
+    totLen += r[0].length;
     var d = new Discord.RichEmbed().setAuthor(this.aliases[0] + ' ' + obj, discord.user.displayAvatarURL);
-    var str = '';
-    for (var l of r) {
-      if  (str.length + l.length < 1020)
-        str += l + '\n';
-      else {
-        d.addField(BLANK, '```' + str + '```');
-        str = l + '\n';
+    console.log(totLen);
+    if (totLen + r.length < 2040) {
+      d.setDescription('```' + r.join('\n') + '```').setTitle(BLANK);
+    } else {
+      var str = '';
+      for (var l of r) {
+        if  (str.length + l.length < 1020)
+          str += l + '\n';
+        else {
+          d.addField(BLANK, '```' + str + '```');
+          str = l + '\n';
+        }
       }
+      if (str != '')
+          d.addField(BLANK, '```' + str + '```');
     }
-    if (str != '')
-        d.addField(BLANK, '```' + str + '```');
     this.send_channel(m.channel, d);
     return true;
   }
